34a229b4f5977f0f2c6ffa261c3f62ce3d268dc0
cesarvazquez-69 <cesar60vazquez@gmail.com>
2026-04-27 13:55:01 -0700

    started writing 1st function

--- functions.py
+++ functions.py
@@ -4,8 +4,33 @@ import unittest
 import math
 import sys
 sys.setrecursionlimit(10**6)
+
+
+@dataclass
 class Tests(unittest.TestCase):
+
 pass
+
+
+
+class Node:
+    value: int
+    next: Optional['Node']
+
+def range(max_exclusive: int) -> Optional['Node']:
+    # Accepts an integer larger than 0 & returns a LinkedList
+    if max_exclusive == 0:
+        return None
+    else:
+        return Node(max_exclusive - 1, range (max_exclusive -1))
+
+    
+
+
+
+
+
+
 
 # Remember from Lab 1: this if statements checks
 # whether this module (ghg.py) is the module
