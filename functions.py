2d99e2c49069aa31f87a999396125ba9ee3e78df
Cesar Vazquez <cesar60vazquez@gmail.com>
2026-04-27 13:20:36 -0700

    functions.py

--- functions.py
+++ functions.py
@@ -6,3 +6,10 @@ import sys
 sys.setrecursionlimit(10**6)
 class Tests(unittest.TestCase):
 pass
+
+# Remember from Lab 1: this if statements checks
+# whether this module (ghg.py) is the module
+# being executed or whether it's just being
+# imported from some other module.
+if (__name__ == '__main__'):
+unittest.main()
